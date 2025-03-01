e4db6d4ecde335136f42f95903f5aea7daaf59d6
lgoodgernz <lgoodgernz@gmail.com>
2025-02-28 21:29:42 -0600

    Update game_data.py

--- game_data.py
+++ game_data.py
@@ -1,2 +1,16 @@
-# Write your code here :-)
-money = 0
+# game_data.py
+
+# Load money from file
+def load_money():
+    try:
+        with open("money.txt", "r") as file:
+            return int(file.read())  # Read the money value
+    except FileNotFoundError:
+        return 0  # Default to 0 if the file does not exist
+
+# Save money to file
+def save_money(money):
+    with open("money.txt", "w") as file:
+        file.write(str(money))  # Save the money value
+
+money = load_money()  # Load money when the game starts
