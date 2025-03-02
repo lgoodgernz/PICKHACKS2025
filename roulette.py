d825052f16d6da18066e619b9a10e29dd31e6624
LWallace2028 <161053707+LWallace2028@users.noreply.github.com>
2025-03-01 18:19:18 -0600

    Update roulette.py

--- roulette.py
+++ roulette.py
@@ -2,15 +2,14 @@ import pygame
 import random
 import game_data
 
-# European Roulette Number Layout
-roulette_numbers = {
-    "RED": {1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36},
-    "BLACK": {2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35},
-    "GREEN": {0}
-}
-
 def get_winning_color(number):
     """Returns the color of the landed number"""
+    roulette_numbers = {
+        "RED": {1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36},
+        "BLACK": {2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35},
+        "GREEN": {0}
+    }
+
     for color, numbers in roulette_numbers.items():
         if number in numbers:
             return color
